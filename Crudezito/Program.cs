d21f63db8f42ffbb716c58031f294365a8bfeb82
Easy-Name <abilonge@yahoo.com>
2024-08-29 13:35:20 -0300

    CRUD done, now separating into funtions

--- Crudezito/Program.cs
+++ Crudezito/Program.cs
@@ -14,14 +14,33 @@ namespace Crudezito
             //string queryStringC = @"insert into EXERCISE (EXERCISE_ID, EXERCISE_NAME, FOCUS_BODY_PART, DIFFICULTY_LEVEL) " +
             //@"values (8, 'Shoulder Press', 'Shoulder', 'Low');";
 
+            string queryStringC = @"insert into EXERCISE (EXERCISE_ID, EXERCISE_NAME, FOCUS_BODY_PART, DIFFICULTY_LEVEL) " +
+@"values (501, 'Test2', 'Shoulder', 'Low');";
+
+            //string queryStringD = @"DELETE FROM dbo.EXERCISE WHERE EXERCISE_NAME='Test';";
+
+            string queryStringU = @"update dbo.EXERCISE SET EXERCISE_ID = 998 , FOCUS_BODY_PART = 'Updated' WHERE EXERCISE_NAME = 'Test2'";
+
+            
+
             using (SqlConnection connection = new(connectionString)) 
             {
                 SqlCommand command = new(queryString, connection);
-                //SqlCommand InsertCommand = new(queryStringC, connection);
+                SqlCommand InsertCommand = new(queryStringC, connection);
+
+                //SqlCommand DeleteCommand = new(queryStringD, connection);
+                SqlCommand UpdateCommand = new(queryStringU, connection);
 
                 try
                 {
                     connection.Open();
+                    InsertCommand.ExecuteNonQuery();
+                    //DeleteCommand.ExecuteNonQuery();
+                    UpdateCommand.ExecuteNonQuery();
+
+
+
+                    
                     SqlDataReader reader = command.ExecuteReader();
                     while (reader.Read())
                     {
